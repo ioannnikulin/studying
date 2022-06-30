2d5349800b34e250c00958e1316b26c481681063
ioann <ioann.nikulin@gmail.com>
2022-06-30 16:32:42 +0300

    input

--- cormen.h
+++ cormen.h
@@ -1,6 +1,10 @@
 #ifndef CORMEN_H_INCLUDED
 #define CORMEN_H_INCLUDED
 
-
+void find_max_subarray_recursive()
+{
+    //cormen edition 3, ch 4.1
+    vector<int> data = {100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
+}
 
 #endif // CORMEN_H_INCLUDED
