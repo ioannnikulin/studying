d7b675cc69e47a02ee847ff06fd839679913005b
ioann <ioann.nikulin@gmail.com>
2022-06-30 16:39:34 +0300

    separate cpp

---
+++ cormen.cpp
@@ -0,0 +1,12 @@
+#include "cormen.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void find_max_subarray_recursive()
+{
+    //cormen edition 3, ch 4.1
+    vector<int> data = {100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "cormen.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    find_max_subarray_recursive();
     return 0;
 }
